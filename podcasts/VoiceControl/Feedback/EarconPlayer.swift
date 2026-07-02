139b8846f07d0aa0ec2f5ba6ed32095f714e49aa
Merlin Ran <merlinran@gmail.com>
2026-07-02 10:01:37 -0400

    feat: add release() and error handling to EarconPlayer
    - Add release() method: stops player, detaches from engine, clears buffers
    - Add proper error logging for missing assets, read failures, buffer creation
    - Make class and key methods open for testability

--- podcasts/VoiceControl/Feedback/EarconPlayer.swift
+++ podcasts/VoiceControl/Feedback/EarconPlayer.swift
@@ -1,7 +1,7 @@
 import AVFoundation
 import PocketCastsUtils
 
-class EarconPlayer {
+open class EarconPlayer {
     private let engine: AVAudioEngine
     private let player = AVAudioPlayerNode()
     private var cachedEarcons: [EarconId: AVAudioPCMBuffer] = [:]
@@ -13,7 +13,7 @@ class EarconPlayer {
         preloadAll()
     }
 
-    func play(_ id: EarconId) {
+    open func play(_ id: EarconId) {
         guard let buffer = cachedEarcons[id] else {
             FileLog.shared.addMessage("[VoiceControl/Earcon] Missing: \(id)")
             return
@@ -26,14 +26,32 @@ class EarconPlayer {
 
     func stop() { player.stop() }
 
+    open func release() {
+        player.stop()
+        engine.detach(player)
+        cachedEarcons.removeAll()
+    }
+
     private func preloadAll() {
         for id in EarconId.allCases {
-            guard let url = Bundle.main.url(forResource: id.rawValue, withExtension: "wav", subdirectory: "earcons"),
-                  let file = try? AVAudioFile(forReading: url),
-                  let buffer = AVAudioPCMBuffer(pcmFormat: file.processingFormat, frameCapacity: AVAudioFrameCount(file.length))
-            else { continue }
-            try? file.read(into: buffer)
-            cachedEarcons[id] = buffer
+            guard let url = Bundle.main.url(forResource: id.rawValue, withExtension: "wav", subdirectory: "earcons") else {
+                FileLog.shared.addMessage("[VoiceControl/Earcon] Missing asset: \(id.rawValue).wav")
+                continue
+            }
+            guard let file = try? AVAudioFile(forReading: url) else {
+                FileLog.shared.addMessage("[VoiceControl/Earcon] Failed to read: \(id.rawValue).wav")
+                continue
+            }
+            guard let buffer = AVAudioPCMBuffer(pcmFormat: file.processingFormat, frameCapacity: AVAudioFrameCount(file.length)) else {
+                FileLog.shared.addMessage("[VoiceControl/Earcon] Failed to create buffer: \(id.rawValue)")
+                continue
+            }
+            do {
+                try file.read(into: buffer)
+                cachedEarcons[id] = buffer
+            } catch {
+                FileLog.shared.addMessage("[VoiceControl/Earcon] Read error for \(id.rawValue): \(error)")
+            }
         }
     }
 }
